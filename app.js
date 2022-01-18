ab4ab5b21db675b39d6779e73e49d8b754794a8d
Igor <igoryashka2001@gmail.com>
2022-01-18 15:50:09 +0300

    remove trash && add logic

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var cors = require('cors')
 
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var translateRouter = require('./routes/translate');
 
 var app = express();
@@ -31,7 +30,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/translate', translateRouter);
 
 // catch 404 and forward to error handler
--- routes/translate.js
+++ routes/translate.js
@@ -1,46 +1,77 @@
 var express = require('express');
 var router = express.Router();
 var axios = require("axios").default;
+var config = require("../config.json");
 
 /* GET home page. */
 router.post('/', function (req, res, next) {
 
+	let translatedCount = 0;
+	let sentCount = 0;
+	let nodeLimit = config.nodeLimit;
+	let apiKey = config.apiKey;
+	let characterLimit = config.characterLimit;
+	let characterCount = 0;
+	let resBody = {};
 
-	console.log('connected')
-	console.log(req.headers)
-	console.log(req.body)
+	console.log('\n\n\nrequest accepted');
+	console.log(req.body);
 
 
 	for (let key in req.body) {
-		if (req.body.hasOwnProperty(key)) {
-			console.log(req.body[key] = ''.padEnd(req.body[key].length, "22222 "))
+		if (req.body.hasOwnProperty(key) && (sentCount !== nodeLimit) && (characterCount <= characterLimit)) {
+			sentCount++;
+			characterCount += req.body[key].length;
+
+			var options = {
+				method: 'POST',
+				url: 'https://microsoft-translator-text.p.rapidapi.com/translate',
+				params: {
+					to: req.headers.to,
+					'api-version': '3.0',
+					profanityAction: 'NoAction',
+					textType: 'plain'
+				},
+				headers: {
+					'content-type': 'application/json',
+					'x-rapidapi-host': 'microsoft-translator-text.p.rapidapi.com',
+					'x-rapidapi-key': apiKey
+				},
+				data: [{Text: req.body[key]}]
+			};
+
+			axios.request(options).then(function (response) {
+				translatedCount++;
+
+				console.log(response.data[0].translations[0].text);
+				resBody[key] = response.data[0].translations[0].text;
+
+
+				if ((translatedCount === sentCount) || Object.keys(req.body).length === translatedCount) {
+					if (!res.headersSent) {
+						console.log("Ответ:", characterCount, "символов", resBody);
+
+						if (translatedCount === nodeLimit) {
+							console.log('nodeLimit')
+						} else if (Object.keys(req.body).length === translatedCount) {
+							console.log('end of Obj')
+						} else if (characterCount >= characterLimit) {
+							console.log('chars limit')
+						}
+						res.json(resBody);
+					}
+				}
+
+			}).catch(function (error) {
+				console.error(error);
+				if (!res.headersSent) {
+					res.json(error);
+				}
+			});
+
+
 		}
 	}
-
-	setTimeout(()=>{res.json(req.body)},500)
-	// var options = {
-	// 	method: 'POST',
-	// 	url: 'https://microsoft-translator-text.p.rapidapi.com/translate',
-	// 	params: {
-	// 		to: 'de',
-	// 		'api-version': '3.0',
-	// 		profanityAction: 'NoAction',
-	// 		textType: 'plain'
-	// 	},
-	// 	headers: {
-	// 		'content-type': 'application/json',
-	// 		'x-rapidapi-host': 'microsoft-translator-text.p.rapidapi.com',
-	// 		'x-rapidapi-key': '82dcf3c090msh3e66c3682cd737cp1c7693jsne9a780818f99'
-	// 	},
-	// 	data: [{Text: 'I would really like to drive your car around the block a few times.'}]
-	// };
-	//
-	// axios.request(options).then(function (response) {
-	// 	console.log(response.data[0].translations);
-	// }).catch(function (error) {
-	// 	console.error(error);
-	// });
-
 
 });
 
